f87f3a5a77b944e644e82ab267085caf3094c0da
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2023-01-07 02:56:20 -0300

    Agrego persistencia de datos con LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,17 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 function App() {
 
 
-const [presupuesto, setPresupuesto] = useState(0);
+const [presupuesto, setPresupuesto] = useState(
+  Number(localStorage.getItem('presupuesto')) ?? 0
+);
 const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
 const [modal, setModal] = useState(false)
 const [animarModal, setAnimarModal] = useState(false)
 
-const [gastos,setGastos] = useState([])
+const [gastos,setGastos] = useState(
+  localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+)
 
 const [gastoEditar, setGastoEditar] = useState({})
 
@@ -29,6 +33,24 @@ useEffect(() => {
     },500)
   }
 }, [gastoEditar])
+
+useEffect(() => {
+  localStorage.setItem('presupuesto',presupuesto ?? 0)
+  // Ponemos en localstorage el presupuesto, si no se encuentra lo inicializamos en 0
+}, [presupuesto])
+
+
+useEffect(() => {
+  const presupuestoLocalStorage = Number(localStorage.getItem('presupuesto') ?? 0)
+
+  if(presupuestoLocalStorage > 0){
+    setIsValidPresupuesto(true)
+  }
+},[])
+
+useEffect(() => {
+  localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+},[gastos])
 
 
 const handleNuevoGasto = () => {
